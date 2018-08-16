9a0deb2d33b5ad700d1d1a2f9417cad72b6ecb22
xala3pa <salazar3pa@gmail.com>
2018-08-16 22:19:35 +0200

    Spring Cloud Stream - simple consumer

---
+++ src/main/java/com/xala3pa/loggingconsumer/LoggingConsumerApplication.java
@@ -0,0 +1,23 @@
+package com.xala3pa.loggingconsumer;
+
+import static java.lang.System.out;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@SpringBootApplication
+@EnableBinding(Sink.class)
+public class LoggingConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingConsumerApplication.class, args);
+	}
+
+	@StreamListener(Sink.INPUT)
+	public void handle(Person person){
+		out.println("Message Received: " + person);
+	}
+}
